5d6ce7143bd0069b42b7815f4a190212f59d2d99
Lila <dalila.analistadesistema@gmail.com>
2024-10-28 17:21:44 -0300

    ...

--- dia4/beau_sele.py
+++ dia4/beau_sele.py
@@ -6,35 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from time import sleep
 
-options = Options()
-options.add_argument('--headless')
+navegador = webdriver.Chrome()
+
+#tratar a
 
 
-#configurando o Selenium e abrindo o navegador
-driver = webdriver.Chrome() # ou webdriver.Firefox() conforme o navegador
-
-driver.get('https://www.airbnb.com')
-
-wait = WebDriverWait(driver, 15)
-
-
-# search_box = wait.until(EC.element_to_be_clickable((By.TAG_NAME, 'input')))
-# search_box.send_keys("São Paulo")
-# search_box.send_keys(Keys.RETURN)
-
-
-
-input_place = driver.find_element(By.TAG_NAME, 'input')
-input_place.send_keys('São Paulo')
-input_place.submit()
-
-
-
-
-# site = BeautifulSoup(driver.page_source, 'html.parser')
-
-# with open('saida.html', 'w', encoding='utf-8') as f:
-    # f.write(site.prettify())
